b04f9ecf3bc0b4fb5cd98071e97f60b12ed6738e
dtycutest <3132210852@qq.com>
2025-02-25 15:36:03 +0800

    第一次作业

---
+++ assignment1/CulTwo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CulTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入第一个数字：");
+            int a=int.Parse(Console.ReadLine());
+            Console.WriteLine("输入第二个数字：");
+            int b= int.Parse(Console.ReadLine());
+            Console.WriteLine("输入运算符：");
+            int opt = Console.Read();
+            int ans=0;
+            if (opt == '+') ans = a + b;
+            if (opt == '-') ans = a - b;
+            if (opt == '*') ans = a * b;
+            if (opt == '/') ans = a / b;
+            if (opt == '%') ans = a % b;
+            Console.WriteLine(ans);
+        }
+    }
+}
