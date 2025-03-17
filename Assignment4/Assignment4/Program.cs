87ade8ed460189e8b0b163fe9caf657d939780a2
dtycutest <3132210852@qq.com>
2025-03-17 16:25:49 +0800

    Assignment 4

---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeListForEach
+{
+    internal class Program
+    {
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T data)
+            {
+                Next = null;
+                this.Data = data;
+            }
+        }
+        public class List<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+            public T sum, min, max;
+            public List()
+            {
+                head=tail = null;
+            }
+            public Node<T> Head()
+            {
+                return this.head;
+            }
+            public void Add(T t)
+            {
+                Node<T> node = new Node<T>(t);
+                if (tail == null)
+                {
+                    tail = head = node;
+                }
+                else
+                {
+                    tail.Next = node;
+                    tail = node;
+                }
+            }
+            public void ForEach(Action<T> action)
+            {
+                Node<T> node = this.head;
+                while (node != null)
+                {
+                    action(node.Data);
+                    node=node.Next;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<int> list= new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.max=int.MinValue;
+            list.min=int.MaxValue;
+            list.sum = 0;
+            list.ForEach(n => Console.WriteLine(n));
+            list.ForEach(n => list.max = Math.Max(list.max, n));
+            list.ForEach(n => list.min = Math.Min(list.min, n));
+            list.ForEach(n => list.sum += n);
+            Console.WriteLine(list.max);
+            Console.WriteLine(list.min);
+            Console.WriteLine(list.sum);
+        }
+    }
+}
---
+++ Assignment4/Clock/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Clock
+{
+    internal class Program
+    {
+        public class TickArgs
+        {
+            public int count;
+        }
+        public delegate void TickHandler(object sender,TickArgs args);
+        public delegate void AlarmHandler(object sender);
+
+
+        public class Clock
+        {
+            public int count;
+            public bool alarmSwitch;
+            public int alarmTime;
+            public event TickHandler OnTick;
+            public event AlarmHandler OnAlarm;
+            public Clock()
+            {
+                count = 0;
+                alarmSwitch = false;
+                alarmTime = 0;
+            }
+            public void Reset()
+            {
+                count = 0;
+                alarmSwitch = false;
+                alarmTime = 0;
+            }
+            public void SetAlarm(int t)
+            {
+                alarmSwitch = true;
+                alarmTime = t;
+            }
+            public void Tick()
+            {
+                count++;
+                TickArgs args = new TickArgs()
+                {
+                    count = this.count
+                };
+                OnTick(this, args);
+                if(alarmSwitch && count == alarmTime)
+                {
+                    OnAlarm(this);
+                }
+            }
+        }
+        static void Clock_OnTick(object sender,TickArgs args)
+        {
+            Console.WriteLine("Tick: " + args.count);
+        }
+        static void Clock_OnAlarm(object sender)
+        {
+            Console.WriteLine("Alarm!!!");
+        }
+
+        private static Clock clock;
+        private static void SetClock()
+        {
+            clock = new Clock();
+            clock.SetAlarm(3);
+            clock.OnTick += new TickHandler(Clock_OnTick);
+            clock.OnAlarm += new AlarmHandler(Clock_OnAlarm);
+        }
+        static void Main(string[] args)
+        {
+            SetClock();
+            for(int i=0;i<5;i++) clock.Tick();
+        }
+    }
+}
